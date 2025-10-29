dbfa2fd4e0573ea763f1794217666dd3e678e056
Nwani Victory <vickywane@gmail.com>
2025-10-29 14:46:12 +0000

    update: remove additonal boilerplate files

--- chrome-extension/manifest.ts
+++ chrome-extension/manifest.ts
@@ -22,17 +22,17 @@ const manifest = {
   manifest_version: 3,
   default_locale: 'en',
   name: '__MSG_extensionName__',
-  browser_specific_settings: {
-    gecko: {
-      id: 'example@example.com',
-      strict_min_version: '109.0',
-    },
-  },
+  // browser_specific_settings: {
+  //   gecko: {
+  //     id: 'example@example.com',
+  //     strict_min_version: '109.0',
+  //   },
+  // },
   version: packageJson.version,
   description: '__MSG_extensionDescription__',
   host_permissions: ['<all_urls>'],
   permissions: ['storage', 'scripting', 'tabs', 'notifications', 'sidePanel'],
-  options_page: 'options/index.html',
+  // options_page: 'options/index.html',
   background: {
     service_worker: 'background.js',
     type: 'module',
@@ -41,44 +41,44 @@ const manifest = {
     default_popup: 'popup/index.html',
     default_icon: 'icon-34.png',
   },
-  chrome_url_overrides: {
-    newtab: 'new-tab/index.html',
-  },
+  // chrome_url_overrides: {
+  //   newtab: 'new-tab/index.html',
+  // },
   icons: {
     '128': 'icon-128.png',
   },
   content_scripts: [
-    {
-      matches: ['http://*/*', 'https://*/*', '<all_urls>'],
-      js: ['content/all.iife.js'],
-    },
-    {
-      matches: ['https://example.com/*'],
-      js: ['content/example.iife.js'],
-    },
-    {
-      matches: ['http://*/*', 'https://*/*', '<all_urls>'],
-      js: ['content-ui/all.iife.js'],
-    },
-    {
-      matches: ['https://example.com/*'],
-      js: ['content-ui/example.iife.js'],
-    },
+    // {
+    //   matches: ['http://*/*', 'https://*/*', '<all_urls>'],
+    //   js: ['content/all.iife.js'],
+    // },
+    // {
+    //   matches: ['https://example.com/*'],
+    //   js: ['content/example.iife.js'],
+    // },
+    // {
+    //   matches: ['http://*/*', 'https://*/*', '<all_urls>'],
+    //   js: ['content-ui/all.iife.js'],
+    // },
+    // {
+    //   matches: ['https://example.com/*'],
+    //   js: ['content-ui/example.iife.js'],
+    // },
     {
       matches: ['http://*/*', 'https://*/*', '<all_urls>'],
       css: ['content.css'],
     },
   ],
-  devtools_page: 'devtools/index.html',
+  // devtools_page: 'devtools/index.html',
   web_accessible_resources: [
     {
       resources: ['*.js', '*.css', '*.svg', 'icon-128.png', 'icon-34.png'],
       matches: ['*://*/*'],
     },
   ],
-  side_panel: {
-    default_path: 'side-panel/index.html',
-  },
+  // side_panel: {
+  //   default_path: 'side-panel/index.html',
+  // },
 } satisfies ManifestType;
 
 export default manifest;
